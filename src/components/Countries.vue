d538327c4cc425d13e82adf69ab792b5579c8cf9
Liakos <liakos.prekas@gmail.com>
2024-11-16 20:46:47 +0200

    chore: organize Countries

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -1,34 +1,33 @@
 <script setup>
-import {onMounted, ref} from 'vue';
-import CountryCard from './CountryCard.vue'
-import axios from 'axios';
-import '../css/countries.scss'
-import '../css/search.scss'
-import NoSearchResults from "@/components/NoSearchResults.vue";
+  import {onMounted, ref} from 'vue';
+  import CountryCard from './CountryCard.vue'
+  import NoSearchResults from "@/components/NoSearchResults.vue";
+  import axios from 'axios';
+  import '../css/countries.scss'
+  import '../css/search.scss'
 
-const countriesData = ref([])
-const searchedCountriesData = ref([])
+  const countriesData = ref([])
+  const searchedCountriesData = ref([])
+  const searchInput = ref('')
+  const API_URL = 'https://restcountries.com/v3.1/'
 
-const searchInput = ref('')
-const API_URL = 'https://restcountries.com/v3.1/'
-
-const fetchFunc = async (url) => {
-  try {
-    const res = await axios.get(url)
-    return res.data
-  }catch(error){
-    return error;
+  const fetchFunc = async (url) => {
+    try {
+      const res = await axios.get(url)
+      return res.data
+    }catch(error){
+      return error;
+    }
   }
-}
 
-const searchFunc = () => {
-  searchedCountriesData.value = countriesData.value.filter( country => country.name.common.toLowerCase().includes(searchInput.value.toLowerCase()) );
-}
+  const searchFunc = () => {
+    searchedCountriesData.value = countriesData.value.filter( country => country.name.common.toLowerCase().includes(searchInput.value.toLowerCase()) );
+  }
 
-onMounted(  async () => {
-  countriesData.value = await fetchFunc(API_URL + "all")
-  searchedCountriesData.value = countriesData.value
-})
+  onMounted(  async () => {
+    countriesData.value = await fetchFunc(API_URL + "all")
+    searchedCountriesData.value = countriesData.value
+  })
 </script>
 
 <template>
